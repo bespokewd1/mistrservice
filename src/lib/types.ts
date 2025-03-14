a5e649d9d68065c9b5c8feb560f90d77dcd9d99b
markanthony1505 <markanthonyinay@yahoo.com>
2025-03-14 23:44:44 +0800

    -push live

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export interface FeatureItem {
+    description: string;
+    icon: string;
+    title: string;
+  }
+  export interface CompatibilityItem {
+    icon: string;
+    title: string;
+    url: string;
+  }
+  
